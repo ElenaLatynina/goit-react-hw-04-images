162a5669304a5a05716825fee66b4a351265c41d
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-18 12:36:51 +0300

    Loader added

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { Progress Bar, ProgressBar } from 'react-loader-spinner';
+import { Spinner } from './Loader.styled';
+
+const Loader = () => {
+    return (
+        <Spinner>
+            <ProgressBar
+                height="80"
+                width="80"
+                ariaLabel="progress-bar-loading"
+                wrapperStyle={{}}
+                wrapperClass="progress-bar-wrapper"
+                borderColor='#F4442E'
+                barColor='#51E5FF'
+            />
+        </Spinner>
+    );
+};
