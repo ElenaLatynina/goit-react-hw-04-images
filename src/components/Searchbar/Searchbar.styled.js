899d2d65bc81ec14575020b0f9575d76b141025f
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-21 22:43:12 +0300

    styles

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -31,8 +31,6 @@ overflow: hidden;
 
 `;
 
-
-
 export const Input = styled.input`
 display: inline-block;
 width: 100%;
@@ -45,26 +43,28 @@ padding-right: 4px;
 `;
 
 export const Label = styled.span`
-font: inherit;
-font-size: 18px;
+position: absolute;
+width: 1px;
+height: 1px;
+padding: 0;
+overflow: hidden;
+clip: rect(0, 0, 0, 0);
+white-space: nowrap;
+clip-path: inset(50%);
+border: 0;
   `;
 
 export const Searchbutton = styled.button`
 display: inline-block;
-width: 48px;
-height: 48px;
-border: 0;
-background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
-background-size: 40%;
-background-repeat: no-repeat;
-background-position: center;
-opacity: 0.6;
-transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
-cursor: pointer;
-outline: none;
-
-:hover{
-    opacity: 1;
-};
-
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+  background-size: 40%;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
 `;
