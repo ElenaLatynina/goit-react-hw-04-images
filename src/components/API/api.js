739d76867c502c868e45c85510cbb78d1ed6dc10
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-23 19:14:33 +0300

    Api

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -1,16 +1,35 @@
 import axios from "axios";
 
-
 export async function receiveData(query, page) {
   const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '29584253-cd90e7f8e2a20a00eedab1d09';
-  const PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
-  const Per_page = 12;
-    
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${page}&per_page=${Per_page}`;
-  const response = await axios.get(url);
-  return response.data;
+  const params = {
+  key: '29584253-cd90e7f8e2a20a00eedab1d09',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 12,
+  query,
+  page,
   
-}
+  };
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    }
+  
+};
+
+export default receiveData;
 
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { List } from './ImageGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem';
 
