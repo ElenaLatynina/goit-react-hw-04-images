2f7487815cd38762df2f408182d069d6a040a6ad
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-25 15:47:13 +0300

    API

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 
-export async function receiveData(query, page) {
+export async function receiveData(searchQuery, page) {
   const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '29584253-cd90e7f8e2a20a00eedab1d09'
   const params = {
-  key: '29584253-cd90e7f8e2a20a00eedab1d09',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
@@ -13,7 +13,7 @@ export async function receiveData(query, page) {
   
   };
  
-  const url = `${BASE_URL}?q=${this.Searchquery}&page=${this.page}`;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}`;
       const response = await axios.get(url, { params });
       return response.data;
 
