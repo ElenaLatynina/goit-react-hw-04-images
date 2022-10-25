ea9cfe95bdc0e822a9043dad5fcd44fe99aee82d
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-25 14:29:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,46 +10,46 @@ import Loader from './Loader';
 import 'react-toastify/dist/ReactToastify.css';
 import Modal from 'components/Modal';
 
-export class App extends Component {
+export class App extends Component{
   state = {
+    page: 1,
     query: '',
-    page:1,
     images: [],
-    isLoading: false,
     largeImageURL: '',
+    isLoading: false,
     totalPages: 0,
-    error:null,
-    
-  };
-
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+    error: null,
   };
 
   loadImages = async (query, page) => {
-    this.setState({ isLoading: true  });
+    this.setState({ isLoading: true });
 
     try {
       const data = await API.receiveData(query, page);
-      
       if (data.hits.length === 0) {
         this.setState({
-          error: { message: `Sorry, there are no images matching your query. Please try again`, },
+          error: {
+            message: `Sorry, there are no images matching your query. Please try again`,
+          },
         });
         return;
       }
       this.setState(prevState => ({
         images: [...prevState.images, ...data.hits],
-        totalPages: data.totalPages,
+        totalPages: data.totalHits,
       }));
-     
-    
-  } catch(error) {
-    this.setState([error]);
+    } catch (error) {
+      this.setState({ error });
     } finally {
       this.setState({ isLoading: false });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
+      this.loadImages(this.state.query,this.state.page);
+    }
   }
-};
 
   handleSearchSubmit = query => {
     this.setState({
@@ -60,35 +60,37 @@ export class App extends Component {
     });
   };
 
-  openModal = (largeImageURL) => { this.setState({ largeImageURL }); };
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
 
-  closeModal = () => { this.setState({ largeImageURL: '' }); };
+  openModal = largeImageURL => {
+    this.setState({ largeImageURL });
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query || 
-      prevState.page !== this.state.page) {
-      this.loadImages(this.setState.query, this.state.page);
-      }
-  }
+  closeModal = () => {
+    this.setState({ largeImageURL: '' });
+  };
 
   render() {
     return (
       <Container>
         <Searchbar onSearch={this.handleSearchSubmit} />
-        {this.state.error && <p>Something went wrong</p>}
-
-        {this.state.images.length > 0 &&( <ImageGallery images={this.state.images} onClick={this.openModal} />)}
+        {this.state.error && <p>Something went wrong:{this.state.error.message}</p>}
+        {this.state.items.length > 0 && (
+          <ImageGallery images={this.state.images} onClick={this.openModal} />
+        )}
         {this.state.isLoading && <Loader />}
-
-        {this.state.page < Math.ceil(this.state.totalPages / 12) && (<Button loadMore={this.loadMore} />)}
-        
-        {this.state.largeImageURL && (<Modal closeModal={this.closeModal}largeImageURL={this.state.largeImageURL} />)}
-       
-        
+        {this.state.page < Math.ceil(this.state.totalPages / 12) && (
+          <Button loadMore={this.loadMore} />
+        )}
+        {this.state.largeImageURL && (
+          <Modal onClose={this.state.closeModal} largeImageURL={this.state.largeImageURL} />
+        )}
       </Container>
-    )
-  };
-
+    );
+  }
 }
 
- 
